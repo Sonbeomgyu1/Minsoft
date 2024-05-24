7d2275c984f2c346d977cea87be3ef2f3f0c836c
sonbeomgyu <sjk6285@naver.com>
2024-05-24 13:34:23 +0900

    오류수정

--- src/main/java/com/mysite/minsoft/config/SecurityConfig.java
+++ src/main/java/com/mysite/minsoft/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.mysite.minsoft.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,51 +16,25 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-        		.antMatchers("/", "/certification", "/login", "/greetingpage", "/history", "/businessdetails", "/solutions", "/itoutsourcingpage",
+            .authorizeRequests((authorizeRequests) -> authorizeRequests
+                .antMatchers("/", "/certification", "/login", "/greetingpage", "/history", "/businessdetails", "/solutions", "/itoutsourcingpage",
                         "/consulting", "/sism", "/recruitmentinfomation", "/welfare", "/contact", "/layout", "/images/**", "/assets/**", 
-                        "/docs/**", "/pages/**", "/sections/**", "/icon/**", "/public/**").permitAll() // 접근 허용 경로 설정
-        		.anyRequest().authenticated())
-        .formLogin((formLogin) -> formLogin
-                .loginPage("/login")
-                .defaultSuccessUrl("/"))
-        .logout((logout) -> logout
+                        "/docs/**", "/pages/**", "/sections/**", "/icon/**", "/public/**").permitAll() // Allow access to these paths without authentication
+                .anyRequest().authenticated()) // Require authentication for any other paths
+            .formLogin((formLogin) -> formLogin
+                .loginPage("/login") // Custom login page
+                .defaultSuccessUrl("/")) // Redirect to "/" after successful login
+            .logout((logout) -> logout
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
-                .invalidateHttpSession(true))
-        .sessionManagement((session) -> session
-        	.invalidSessionUrl("/")); // 세션이 유효하지 않을 때 메인 페이지로 리다이렉트
+                .invalidateHttpSession(true)) // Invalidate session on logout
+            .sessionManagement((session) -> session
+                .invalidSessionUrl("/")); // Redirect to main page when the session is invalid
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/certification", "/greetingpage", "/history", "/businessdetails", "/solutions", "/itoutsourcingpage",
-                        "/consulting", "/sism", "/recruitmentinfomation", "/welfare", "/contact", "/layout","/notice","/presentation", "/notice/new", "/images/**", "/assets/**", 
-                        "/docs/**", "/pages/**", "/sections/**", "/icon/**", "/public/**").permitAll() // 접근 허용 경로 설정
-                .anyRequest().authenticated() // 나머지 경로는 인증 필요
-                .and()
-            .formLogin()
-                .loginPage("/login") // 커스텀 로그인 페이지 설정
-                .defaultSuccessUrl("/", true) // 로그인 성공 시 이동할 페이지 설정
-                .successHandler(authenticationSuccessHandler()) // 로그인 성공 핸들러 설정
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
-
-
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
