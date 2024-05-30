fbbdafb5f8e616f3c6dd80260a7b6c1a58be2979
sonbeomgyu <sjk6285@naver.com>
2024-05-30 16:39:37 +0900

    현재페이지유지 수정중

--- src/main/java/com/mysite/minsoft/config/SecurityConfig.java
+++ src/main/java/com/mysite/minsoft/config/SecurityConfig.java
@@ -9,39 +9,44 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
 
-                .authorizeRequests(authorizeRequests -> authorizeRequests
-                        .antMatchers("/", "/certification", "/login", "/signup", "/greetingpage", "/history",
-                                "/businessdetails", "/solutions", "/itoutsourcingpage",
-                                "/consulting", "/sism", "/recruitmentinfomation", "/welfare","/contact","/board",
-                                 "/check-db-connection", "/layout", "/images/**", "/assets/**",
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	        .authorizeRequests(authorizeRequests -> authorizeRequests
+	            .antMatchers("/", "/certification", "/login", "/signup", "/greetingpage", "/history",
+	                    "/businessdetails", "/solutions", "/itoutsourcingpage",
+	                    "/consulting", "/sism", "/recruitmentinfomation", "/welfare", "/contact", "/board",
+	                    "/check-db-connection", "/layout", "/images/**", "/assets/**",
+	                    "/docs/**", "/pages/**", "/sections/**", "/icon/**", "/public/**")
+	            .permitAll() // Allow access to these paths without authentication
+	            .anyRequest().authenticated()) // Require authentication for any other paths
+	        .formLogin(formLogin -> formLogin
+	            .loginPage("/login") // Custom login page
+	            .successHandler(savedRequestAwareAuthenticationSuccessHandler()) // Custom success handler
+	            .permitAll()) // Allow everyone to see the login page
+	        .logout(logout -> logout
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	            .logoutSuccessUrl("/")
+	            .invalidateHttpSession(true)) // Invalidate session on logout
+	        .csrf(csrf -> csrf.disable()) // Disable CSRF protection if necessary
+	        .sessionManagement(session -> session.invalidSessionUrl("/")); // Redirect to main page when the session is invalid
+	    return http.build();
+	}
 
-                                "/docs/**", "/pages/**", "/sections/**", "/icon/**", "/public/**")
-                        .permitAll() // Allow access to these paths without authentication
-                        .anyRequest().authenticated()) // Require authentication for any other paths
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login") // Custom login page
-                        .defaultSuccessUrl("/") // Redirect to "/" after successful login
-                        .permitAll()) // Allow everyone to see the login page
-                .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)) // Invalidate session on logout
-                .csrf(csrf -> csrf
-                        .disable()) // Disable CSRF protection if necessary
-                .sessionManagement(session -> session
-                        .invalidSessionUrl("/")); // Redirect to main page when the session is invalid
-        return http.build();
-    }
+	@Bean
+	 SavedRequestAwareAuthenticationSuccessHandler savedRequestAwareAuthenticationSuccessHandler() {
+	    SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+	    successHandler.setUseReferer(true); // 현재 페이지로 리다이렉트하기 위해 Referer를 사용합니다.
+	    return successHandler;
+	}
 
     @Bean
     PasswordEncoder passwordEncoder() {
