bfb21eb8286153f1d07fc5e78e228c8439d861de
sonbeomgyu <sjk6285@naver.com>
2024-05-30 14:17:27 +0900

    board

--- src/main/java/com/mysite/minsoft/board/BoardController.java
+++ src/main/java/com/mysite/minsoft/board/BoardController.java
@@ -1,12 +1,14 @@
 package com.mysite.minsoft.board;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class BoardController {
 
 	
-	 @GetMapping("/bo") //공지사항 컨트롤러
+	 @GetMapping("/board") //공지사항 컨트롤러
 	    public String board() {
-	        return "bo"; 
+	        return "board"; 
 	    }
 }
