436d190f98149d4eccaff69fe07f870f5676feb9
cheongha12 <cheong.9396@gmail.com>
2024-05-31 16:44:07 +0900

    board Entity 클래스 생성
    나머지 service, repository, controller 생성예정

--- src/main/java/com/mysite/minsoft/board/Board.java
+++ src/main/java/com/mysite/minsoft/board/Board.java
@@ -1,17 +1,39 @@
 package com.mysite.minsoft.board;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import com.mysite.minsoft.login.model.SiteUser;
+
+import java.io.Serializable;
 
 @Entity
-@Table(name = "BORAD")
-public class Board {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+@Getter
+@Setter
+@Table(name = "BOARD")
+public class Board implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "AUTHOR", referencedColumnName = "NAME")
+    private SiteUser author;
+
+    @Column(name = "ISPUBLIC", nullable = false)
+    private boolean isPublic;
+
+    @Lob
+    @Column(name = "CONTENT", nullable = false)
+    private String content;
 }
