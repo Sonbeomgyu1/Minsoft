b03d758a746456cb307a4884200b9379f299cadc
sonbeomgyu <sjk6285@naver.com>
2024-05-31 11:41:30 +0900

    글쓰기페이지 완료
    1

--- src/main/java/com/mysite/minsoft/board/BoardController.java
+++ src/main/java/com/mysite/minsoft/board/BoardController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
 	
-	 @GetMapping("/board") //공지사항 컨트롤러
+	 @GetMapping("/board") //공지사항 main 컨트롤러
 	    public String board() {
 	        return "board"; 
 	    }
 	 
-	 @GetMapping("/boarddetail") //공지사항 컨트롤러
+	 @GetMapping("/boarddetail") //공지사항 상세페에지 컨트롤러
 	    public String boardDetail() {
 	        return "board_detail"; 
+	    }	
+	 
+	 @GetMapping("/boardwriting") //공지사항 글쓰기 컨트롤러
+	    public String boardwriting() {
+	        return "board_writing"; 
 	    }
 }
