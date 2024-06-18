a8250573f8fe6f8ec463c598e21d861e325418c6
Cher737 <star10978@naver.com>
2024-06-18 11:40:19 +0900

    06.18 war 배포용 수정
    build.gradle
    MinsoftApplication.java 파일 2개 수정

--- src/main/java/com/mysite/minsoft/MinsoftApplication.java
+++ src/main/java/com/mysite/minsoft/MinsoftApplication.java
@@ -2,9 +2,20 @@ package com.mysite.minsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MinsoftApplication {
+public class MinsoftApplication extends SpringBootServletInitializer{ //war 배포용
+
+//@SpringBootApplication
+//public class MinsoftApplication { 기존코드
+	
+    @Override //war 배포용
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MinsoftApplication.class);
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinsoftApplication.class, args);
